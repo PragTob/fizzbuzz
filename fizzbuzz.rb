ab88b1507d4b9d6c8d28bce12f768a3373cadd19
Tobias Pfeiffer <pragtob@gmail.com>
2018-01-09 23:10:33 +0100

    Ruby

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(n)
+  if (n % 15).zero?
+    "FizzBuzz"
+  elsif (n % 5).zero?
+    "Buzz"
+  elsif (n % 3).zero?
+    "Fizz"
+  else
+    n
+  end
+end
+
+(1..100).each do |n|
+  puts fizzbuzz(n)
+end
