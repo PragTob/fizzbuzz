ee680457f85e1b87e7bee662a0058ad3fc4e6aac
Tobias Pfeiffer <pragtob@gmail.com>
2018-01-10 00:01:50 +0100

    And JS

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+const fizzBuzz = n => {
+    if (n % 15 === 0) {
+        return 'FizzBuzz';
+    } else if (n % 3 === 0) {
+        return 'Fizz';
+    } else if (n % 5 === 0) {
+        return 'Buzz'
+    } else {
+        return n
+    }
+};
+
+for (let n = 1; n <= 100; n += 1) {
+    console.log(fizzBuzz(n));
+}
