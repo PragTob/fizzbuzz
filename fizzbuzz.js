c71139dacdf7c5a969a76f35e8f0b2e3c77693f8
Tobias Pfeiffer <pragtob@gmail.com>
2018-01-10 20:54:32 +0100

    improve JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,12 @@
 const fizzBuzz = n => {
     if (n % 15 === 0) {
-        return 'FizzBuzz';
+        return "FizzBuzz";
     } else if (n % 3 === 0) {
-        return 'Fizz';
+        return "Fizz";
     } else if (n % 5 === 0) {
-        return 'Buzz'
+        return "Buzz";
     } else {
-        return n
+        return n;
     }
 };
 
